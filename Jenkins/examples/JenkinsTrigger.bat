2c930de679af601dd070766ce84b88662f139c9b
ziu7wx <jin.zhu5@bosch.com>
2023-02-07 15:47:19 +0800

    chore: remove tokens

--- Jenkins/examples/JenkinsTrigger.bat
+++ Jenkins/examples/JenkinsTrigger.bat
@@ -1,1 +1,1 @@
-curl -I -u admin:11e8f3dfa2165b946af5484f7f23205cef http://wx-c-001tf.wx.apac.bosch.com:1111/job/Trigger/build?token=myJobToken
+curl -I -u admin:admintoken http://host:1111/job/Trigger/build?token=projecttoken
